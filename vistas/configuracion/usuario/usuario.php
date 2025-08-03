1d713ccf634d0a818f9cf8113ffac9be470fdd71
CalitoTech <carlosdanielnavas26@gmail.com>
2025-08-03 13:01:23 -0400

    correcion en la ruta a tablas.js

--- vistas/configuracion/usuario/usuario.php
+++ vistas/configuracion/usuario/usuario.php
@@ -176,7 +176,7 @@ if ($alert) {
 </section>
 
 <?php include '../../layouts/footer.php'; ?>
-<script src="../.././assets/js/tablas.js"></script>
+<script src="../../../assets/js/tablas.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="../../../assets/js/reportes.js"></script>
 <script>
