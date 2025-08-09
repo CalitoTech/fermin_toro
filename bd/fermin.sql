474fc93f834eb7aec41d8bee5c727ba124d58217
CalitoTech <carlosdanielnavas26@gmail.com>
2025-08-09 16:39:49 -0400

    correciones en la base de datos

--- bd/fermin.sql
+++ bd/fermin.sql
@@ -11,11 +11,12 @@ INSERT INTO `perfil` (`IdPerfil`, `nombre_perfil`) VALUES
 (2, 'Docente'),
 (3, 'Estudiante'),
 (4, 'Representante'),
-(5, 'Director'),
-(6, 'Control de estudios'),
-(7, 'Coordinador Inicial'),
-(8, 'Coordinador Primaria'),
-(9, 'Coordinador Media General');
+(5, 'Contacto de Emergencia'),
+(6, 'Director'),
+(7, 'Control de estudios'),
+(8, 'Coordinador Inicial'),
+(9, 'Coordinador Primaria'),
+(10, 'Coordinador Media General');
 
 CREATE TABLE tabla (
     IdTabla int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -50,6 +51,26 @@ CREATE TABLE requisito (
     FOREIGN KEY (IdNivel) REFERENCES nivel(IdNivel)
 );
 
+CREATE TABLE aula (
+    IdAula int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    IdNivel int NOT NULL,
+    aula varchar(50) NOT NULL,
+    capacidad int NOT NULL,
+    FOREIGN KEY (IdNivel) REFERENCES nivel(IdNivel)
+);
+
+INSERT INTO `aula` (`IdAula`, `IdNivel`, `aula`, `capacidad`) VALUES
+(1, 1, 'Aula 1', 35), 
+(2, 1, 'Aula 2', 35), 
+(3, 1, 'Aula 3', 35),
+(4, 1, 'Aula 4', 35), 
+(5, 1, 'Aula 5', 35), 
+(6, 1, 'Aula 6', 35), 
+(7, 1, 'Aula 7', 35), 
+(8, 1, 'Aula 8', 35), 
+(9, 1, 'Aula 9', 35),
+(10, 1, 'Aula 10', 35);
+
 CREATE TABLE curso (
     IdCurso int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     curso varchar(50) NOT NULL,
@@ -79,60 +100,76 @@ CREATE TABLE seccion (
 );
 
 INSERT INTO `seccion` (`IdSeccion`, `seccion`) VALUES
-(1, 'A'), (2, 'B'), (3, 'C');
+(1, 'A'), (2, 'B'), (3, 'C'), (4, 'Inscripcion');
 
-CREATE TABLE aula (
-    IdAula int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    capacidad int NULL,
-    IdCurso int NULL,
-    IdSeccion int NULL,
+CREATE TABLE curso_seccion (
+    IdCurso_Seccion int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cantidad_estudiantes int NOT NULL,
+    IdCurso int NOT NULL,
+    IdSeccion int NOT NULL,
+    IdAula int NOT NULL,
+    FOREIGN KEY (IdAula) REFERENCES aula(IdAula),
     FOREIGN KEY (IdCurso) REFERENCES curso(IdCurso),
     FOREIGN KEY (IdSeccion) REFERENCES seccion(IdSeccion)
 );
 
-INSERT INTO `aula` (`IdAula`, `capacidad`, `IdCurso`, `IdSeccion`) VALUES
-(1, 30, 1, 1),
-(2, 30, 2, 1),
-(3, 30, 3, 1),
-(4, 30, 4, 1),
-(5, 30, 5, 1),
-(6, 30, 6, 1),
-(7, 30, 7, 1),
-(8, 30, 8, 1),
-(9, 30, 9, 1),
-(10, 30, 10, 1),
-(11, 30, 11, 1),
-(12, 30, 12, 1),
-(13, 30, 13, 1),
-(14, 30, 14, 1),
-(15, 30, 1, 2),
-(16, 30, 2, 2),
-(17, 30, 3, 2),
-(18, 30, 4, 2),
-(19, 30, 5, 2),
-(20, 30, 6, 2),
-(21, 30, 7, 2),
-(22, 30, 8, 2),
-(23, 30, 9, 2),
-(24, 30, 10, 2),
-(25, 30, 11, 2),
-(26, 30, 12, 2),
-(27, 30, 13, 2),
-(28, 30, 14, 2),
-(29, 30, 1, 3),
-(30, 30, 2, 3),
-(31, 30, 3, 3),
-(32, 30, 4, 3),
-(33, 30, 5, 3),
-(34, 30, 6, 3),
-(35, 30, 7, 3),
-(36, 30, 8, 3),
-(37, 30, 9, 3),
-(38, 30, 10, 3),
-(39, 30, 11, 3),
-(40, 30, 12, 3),
-(41, 30, 13, 3),
-(42, 30, 14, 3);
+INSERT INTO `curso_seccion` (`IdCurso_Seccion`, `cantidad_estudiantes`, `IdCurso`, `IdSeccion`, `IdAula`) VALUES
+(1, 0, 1, 1),
+(2, 0, 2, 1),
+(3, 0, 3, 1),
+(4, 0, 4, 1),
+(5, 0, 5, 1),
+(6, 0, 6, 1),
+(7, 0, 7, 1),
+(8, 0, 8, 1),
+(9, 0, 9, 1),
+(10, 0, 10, 1),
+(11, 0, 11, 1),
+(12, 0, 12, 1),
+(13, 0, 13, 1),
+(14, 0, 14, 1),
+(15, 0, 1, 2),
+(16, 0, 2, 2),
+(17, 0, 3, 2),
+(18, 0, 4, 2),
+(19, 0, 5, 2),
+(20, 0, 6, 2),
+(21, 0, 7, 2),
+(22, 0, 8, 2),
+(23, 0, 9, 2),
+(24, 0, 10, 2),
+(25, 0, 11, 2),
+(26, 0, 12, 2),
+(27, 0, 13, 2),
+(28, 0, 14, 2),
+(29, 0, 1, 3),
+(30, 0, 2, 3),
+(31, 0, 3, 3),
+(32, 0, 4, 3),
+(33, 0, 5, 3),
+(34, 0, 6, 3),
+(35, 0, 7, 3),
+(36, 0, 8, 3),
+(37, 0, 9, 3),
+(38, 0, 10, 3),
+(39, 0, 11, 3),
+(40, 0, 12, 3),
+(41, 0, 13, 3),
+(42, 0, 14, 3),
+(43, 0, 1, 4),
+(44, 0, 2, 4),
+(45, 0, 3, 4),
+(46, 0, 4, 4),
+(47, 0, 5, 4),
+(48, 0, 6, 4),
+(49, 0, 7, 4),
+(50, 0, 8, 4),
+(51, 0, 9, 4),
+(52, 0, 10, 4),
+(53, 0, 11, 4),
+(54, 0, 12, 4),
+(55, 0, 13, 4),
+(56, 0, 14, 4);
 
 CREATE TABLE sexo (
     IdSexo int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -159,31 +196,33 @@ CREATE TABLE nacionalidad (
 INSERT INTO `nacionalidad` (`IdNacionalidad`, `nacionalidad`) VALUES
 (1, 'V'), (2, 'E');
 
-
-CREATE TABLE condicion (
-    IdCondicion int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    condicion varchar(50) NOT NULL
+CREATE TABLE tipo_status (
+    IdTipo_Status int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tipo_status varchar(50) NOT NULL
 );
 
-INSERT INTO `condicion` (`IdCondicion`, `condicion`) VALUES
-(1, 'Activo'),
-(2, 'Inactivo'),
-(3, 'Jubilado'),
-(4, 'Reposo'),
-(5, 'Vacaciones'),
-(6, 'Graduado');
+INSERT INTO `tipo_status` (`IdTipo_Status`, `tipo_status`) VALUES
+(1, 'Persona'), (2, 'Inscripción');
 
-CREATE TABLE estado_inscripcion (
-    IdEstado_Inscripcion int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    estado_inscripcion varchar(50) NOT NULL
+CREATE TABLE status (
+    IdStatus int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    IdTipo_Status int NOT NULL,
+    status varchar(50) NOT NULL,
+    FOREIGN KEY (IdTipo_Status) REFERENCES tipo_status(IdTipo_Status)
 );
 
-INSERT INTO `estado_inscripcion` (`IdEstado_Inscripcion`, `estado_inscripcion`) VALUES
-(1, 'Pendiente de aprobación'),
-(2, 'Aprobada para reunión'),
-(3, 'Rechazada'),
-(4, 'En espera de pago'),
-(5, 'Inscrito');
+INSERT INTO `status` (`IdStatus`, `IdTipo_Status`, `status`) VALUES
+(1, 1, 'Activo'),
+(2, 1, 'Inactivo'),
+(3, 1, 'Jubilado'),
+(4, 1, 'Reposo'),
+(5, 1, 'Vacaciones'),
+(6, 1, 'Graduado'),
+(7, 2, 'Pendiente de aprobación'),
+(8, 2, 'Aprobada para reunión'),
+(9, 2, 'Rechazada'),
+(10, 2, 'En espera de pago'),
+(11, 2, 'Inscrito');
 
 CREATE TABLE parentesco (
     IdParentesco int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -206,25 +245,31 @@ CREATE TABLE persona (
     nombre varchar(50) NOT NULL,
     apellido varchar(50) NOT NULL,
     fecha_nacimiento date NULL,
-    fecha_egreso date NULL,
     correo varchar(50) NULL,
     usuario varchar(50) NULL,
     password varchar(1000) NULL,
     direccion varchar(555) NULL,
-    lugar_trabajo varchar(50) NULL,
     IdSexo int NULL,
     IdUrbanismo int NULL,
-    IdCondicion int NULL,
-    IdAula int NULL,
+    IdStatus int NULL,
     FOREIGN KEY (IdNacionalidad) REFERENCES nacionalidad(IdNacionalidad),
     FOREIGN KEY (IdUrbanismo) REFERENCES urbanismo(IdUrbanismo),
-    FOREIGN KEY (IdCondicion) REFERENCES condicion(IdCondicion),
-    FOREIGN KEY (IdSexo) REFERENCES sexo(IdSexo),
-    FOREIGN KEY (IdAula) REFERENCES aula(IdAula)
+    FOREIGN KEY (IdStatus) REFERENCES status(IdStatus),
+    FOREIGN KEY (IdSexo) REFERENCES sexo(IdSexo)
 );
 
-INSERT INTO `persona` (`IdPersona`, `IdNacionalidad`, `cedula`, `nombre`, `apellido`, `fecha_nacimiento`, `fecha_egreso`, `correo`, `usuario`, `password`, `direccion`, `lugar_trabajo`, `IdSexo`, `IdUrbanismo`, `IdCondicion`, `IdAula`) VALUES
+INSERT INTO `persona` (`IdPersona`, `IdNacionalidad`, `cedula`, `nombre`, `apellido`, `fecha_nacimiento`, `fecha_egreso`, `correo`, `usuario`, `password`, `direccion`, `lugar_trabajo`, `IdSexo`, `IdUrbanismo`, `IdStatus`, `IdCurso_Seccion`) VALUES
 (1, 1, 30588094, 'Carlos', 'Navas', '2004-10-26', NULL, 'carlosdanielnavas26@gmail.com', 'carlos', '$2y$10$DeA8v8DgHihCe2aKBW4qZuwtITen6EM5W4OdQKoZoQHqWsBCuOM/2', 'Av. Sucre, Calle 3, Casa #152', 'CorpoEureka', 1, 1, NULL, NULL);
+
+CREATE TABLE egreso (
+    IdEgreso int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fecha_egreso date NOT NULL,
+    motivo varchar(255) DEFAULT NULL,
+    IdPersona int NOT NULL,
+    IdStatus int NOT NULL,
+    FOREIGN KEY (IdStatus) REFERENCES status(IdStatus),
+    FOREIGN KEY (IdPersona) REFERENCES persona(IdPersona)
+);
 
 CREATE TABLE fecha_escolar (
     IdFecha_Escolar int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -237,28 +282,17 @@ INSERT INTO `fecha_escolar` (`IdFecha_Escolar`, `fecha_escolar`, `activa`) VALUE
 (2, '2024-2025', False),
 (3, '2025-2026', True);
 
-CREATE TABLE historial_aula (
-    IdHistorial_Aula int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    IdPersona int NOT NULL,
-    IdAula int NOT NULL,
-    IdFecha_Escolar int NOT NULL,
-    fecha_ingreso date NOT NULL,
-    FOREIGN KEY (IdPersona) REFERENCES persona(IdPersona),
-    FOREIGN KEY (IdAula) REFERENCES aula(IdAula),
-    FOREIGN KEY (IdFecha_Escolar) REFERENCES fecha_escolar(IdFecha_Escolar)
+CREATE TABLE tipo_discapacidad (
+    IdTipo_Discapacidad int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tipo_discapacidad varchar(50) NOT NULL
 );
 
-CREATE TABLE dificultad (
-    IdDificultad int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    visual BOOLEAN NOT NULL DEFAULT FALSE,
-    auditiva BOOLEAN NOT NULL DEFAULT FALSE,
-    motora BOOLEAN NOT NULL DEFAULT FALSE,
-    es_alergico BOOLEAN NOT NULL DEFAULT FALSE,
-    alergia varchar(50) DEFAULT NULL,
-    tiene_enfermedad BOOLEAN NOT NULL DEFAULT FALSE,
-    enfermedad varchar(50) DEFAULT NULL,
+CREATE TABLE discapacidad (
+    IdDiscapacidad int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     IdPersona int NOT NULL,
-    FOREIGN KEY (IdPersona) REFERENCES persona(IdPersona)
+    IdTipo_Discapacidad int NOT NULL,
+    FOREIGN KEY (IdPersona) REFERENCES persona(IdPersona),
+    FOREIGN KEY (IdTipo_Discapacidad) REFERENCES tipo_discapacidad(IdTipo_Discapacidad)
 );
 
 CREATE TABLE representante (
@@ -267,6 +301,7 @@ CREATE TABLE representante (
     IdParentesco int NOT NULL,
     IdEstudiante int NOT NULL,
     ocupacion varchar(50) DEFAULT NULL,
+    lugar_trabajo varchar(50) NULL,
     FOREIGN KEY (IdParentesco) REFERENCES parentesco(IdParentesco),
     FOREIGN KEY (IdPersona) REFERENCES persona(IdPersona),
     FOREIGN KEY (IdEstudiante) REFERENCES persona(IdPersona)
@@ -282,8 +317,8 @@ CREATE TABLE inscripcion (
     responsable_inscripcion int NOT NULL,
     IdFecha_Escolar int NOT NULL,
     IdEstado_Inscripcion int NOT NULL,
-    IdCurso int NOT NULL,
-    FOREIGN KEY (IdCurso) REFERENCES curso(IdCurso),
+    IdCurso_Seccion int NOT NULL,
+    FOREIGN KEY (IdCurso_Seccion) REFERENCES curso_seccion(IdCurso_Seccion),
     FOREIGN KEY (IdEstado_Inscripcion) REFERENCES estado_inscripcion(IdEstado_Inscripcion),
     FOREIGN KEY (IdFecha_Escolar) REFERENCES fecha_escolar(IdFecha_Escolar),
     FOREIGN KEY (IdEstudiante) REFERENCES persona(IdPersona),
@@ -354,18 +389,18 @@ CREATE TABLE horario (
     IdBloque int NOT NULL,
     IdDia int NOT NULL,
     IdPersona int NOT NULL,
-    IdAula int NOT NULL,
+    IdCurso_Seccion int NOT NULL,
     IdFecha_Escolar int NOT NULL,
     FOREIGN KEY (IdFecha_Escolar) REFERENCES fecha_escolar(IdFecha_Escolar),
     FOREIGN KEY (IdDia) REFERENCES dia(IdDia),
     FOREIGN KEY (IdBloque) REFERENCES bloque(IdBloque),
     FOREIGN KEY (IdMateria) REFERENCES materia(IdMateria),
     FOREIGN KEY (IdPersona) REFERENCES persona(IdPersona),
-    FOREIGN KEY (IdAula) REFERENCES aula(IdAula)
+    FOREIGN KEY (IdCurso_Seccion) REFERENCES curso_seccion(IdCurso_Seccion)
 );
 
 CREATE TABLE tipo_grupo_creacion (
-    IdTipoGrupo int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    IdTipo_Grupo int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre_grupo varchar(50) NOT NULL,
     descripcion varchar(255) DEFAULT NULL,
     capacidad_maxima int NOT NULL,
@@ -376,14 +411,14 @@ CREATE TABLE tipo_grupo_creacion (
 
 CREATE TABLE grupo_creacion (
     IdGrupo_Creacion int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    IdTipoGrupo int NOT NULL,
+    IdTipo_Grupo int NOT NULL,
     IdProfesor int NOT NULL,
-    IdAula int NOT NULL,
+    IdCurso_Seccion int NOT NULL,
     IdFecha_Escolar int NOT NULL,
     cupos_disponibles int NOT NULL,
-    FOREIGN KEY (IdTipoGrupo) REFERENCES tipo_grupo_creacion(IdTipoGrupo),
+    FOREIGN KEY (IdTipo_Grupo) REFERENCES tipo_grupo_creacion(IdTipo_Grupo),
     FOREIGN KEY (IdProfesor) REFERENCES persona(IdPersona),
-    FOREIGN KEY (IdAula) REFERENCES aula(IdAula),
+    FOREIGN KEY (IdCurso_Seccion) REFERENCES curso_seccion(IdCurso_Seccion),
     FOREIGN KEY (IdFecha_Escolar) REFERENCES fecha_escolar(IdFecha_Escolar)
 );
 
